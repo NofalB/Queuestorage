739678efcb93c980ae75f5903b340f6444af12c7
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2021-09-12 19:52:14 +0200

    final

--- ConsoleAppQueue/IQueueStorage.cs
+++ ConsoleAppQueue/IQueueStorage.cs
@@ -7,5 +7,7 @@ namespace ConsoleAppQueue
         Task CreateMessage(string message);
         Task<string> PeekMessage();
         Task DeleteMessage();
+
+       // https://www.infoworld.com/article/3628229/how-to-work-with-azure-queue-storage-in-csharp.html#jump
     }
 }
